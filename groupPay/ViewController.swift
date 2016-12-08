74f2fdfe5310d926a431794d0ce4e32ff1217f77
FORD ADAM <758743@ehs-s-232-28.local>
2016-12-08 10:39:06 -0600

    Initial Commit

---
+++ groupPay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  groupPay
+//
+//  Created by FORD, ADAM on 12/8/16.
+//  Copyright © 2016 FORD, ADAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
