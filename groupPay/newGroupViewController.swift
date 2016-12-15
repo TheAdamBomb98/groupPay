4dc86fc66cdba4afe904625c11d4b124c1fd958c
FORD ADAM <758743@ehs-s-232-28>
2016-12-15 11:04:23 -0600

    Added the New Group GUI

--- groupPay/newGroupViewController.swift
+++ groupPay/newGroupViewController.swift
@@ -1,25 +1,25 @@
 //
-//  ViewController.swift
+//  newGroupViewController.swift
 //  groupPay
 //
-//  Created by FORD, ADAM on 12/8/16.
+//  Created by FORD, ADAM on 12/15/16.
 //  Copyright © 2016 FORD, ADAM. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class newGroupViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
