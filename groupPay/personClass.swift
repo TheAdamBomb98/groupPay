0bf8d24130f534a5a9badd9780289e9c97eaba2a
BLANCHARD JENNIFER <843121@eh-s-aviat-18.district196.org>
2016-12-16 10:55:02 -0600

    changed classes to NS coding

--- groupPay/personClass.swift
+++ groupPay/personClass.swift
@@ -1,23 +1,51 @@
-//
-//  personClass.swift
-//  groupPay
-//
-//  Created by FORD, ADAM on 12/13/16.
-//  Copyright © 2016 FORD, ADAM. All rights reserved.
-//
-
-import Foundation
-
-class Person {
+  //
+  //  personClass.swift
+  //  groupPay
+  //
+  //  Created by FORD, ADAM on 12/13/16.
+  //  Copyright © 2016 FORD, ADAM. All rights reserved.
+  //
+  
+  import Foundation
+  import UIKit
+  import os.log
+  
+  class Person: NSObject, NSCoding {
+    
+    //MARK: Properties
+    
     var name: String
     var totalPlusMinus: Double
     var transactions: [Receipt]
     var currentPay: Double
     var currentPlusMinus: Double
-    //phoneNumber and email will need to be changed to accomodate for actually sending notifications, Also may need to be optional...
+    //phoneNumber and email will need to be changed to accomodate for actually sending notifications
+    //Also may need to be optional...
     var phoneNumber: String
     var email: String
+    
+    //MARK: Archiving Paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("persons")
+    
+    //MARK: Types
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let totalPlusMinus = "totalPlusMinus"
+        static let transactions = "transactions"
+        static let currentPay = "currentPay"
+        static let currentPlusMinus = "currentPlusMinus"
+        static let phoneNumber = "phoneNumber"
+        static let email = "email"
+    }
+    
+    //MARK: Initialization
+    
     init( name: String , totalPlusMinus: Double , transactions: [Receipt] , currentPay: Double , currentPlusMinus: Double , phoneNumber: String , email: String){
+        
+        //initalize stored properties
+        
         self.name = name
         self.totalPlusMinus = totalPlusMinus
         self.transactions = transactions
@@ -26,4 +54,35 @@ class Person {
         self.phoneNumber = phoneNumber
         self.email = email
     }
-}
+    
+    //MARK: NSCoding
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(totalPlusMinus, forKey: PropertyKey.totalPlusMinus)
+        aCoder.encode(transactions, forKey: PropertyKey.transactions)
+        aCoder.encode(currentPay, forKey: PropertyKey.currentPay)
+        aCoder.encode(currentPlusMinus, forKey: PropertyKey.currentPlusMinus)
+        aCoder.encode(phoneNumber, forKey: PropertyKey.phoneNumber)
+        aCoder.encode(email, forKey: PropertyKey.email)
+    }
+    
+    //...
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        
+        
+        let name = aDecoder.decodeObject(forKey: PropertyKey.name)
+        let totalPlusMinus = aDecoder.decodeObject(forKey: PropertyKey.totalPlusMinus)
+        let transactions = aDecoder.decodeObject(forKey: PropertyKey.transactions)
+        let currentPay = aDecoder.decodeObject(forKey: PropertyKey.currentPay)
+        let currentPlusMinus = aDecoder.decodeObject(forKey: PropertyKey.currentPlusMinus)
+        let phoneNumber = aDecoder.decodeObject(forKey: PropertyKey.phoneNumber)
+        let email = aDecoder.decodeObject(forKey: PropertyKey.email)
+        
+        
+        // Must call designated initializer.
+        self.init(name: name as! String, totalPlusMinus: totalPlusMinus as! Double, transactions: transactions as! [Receipt], currentPay: currentPay as! Double, currentPlusMinus: currentPlusMinus as! Double, phoneNumber: phoneNumber as! String, email: email as! String)
+        
+    }
+  }
--- groupPay/receiptClass.swift
+++ groupPay/receiptClass.swift
@@ -7,17 +7,62 @@
 //
 
 import Foundation
+import UIKit
+import os.log
 
-class Receipt{
+class Receipt: NSObject, NSCoding {
+   
+    //MARK: Properties
+    
     var amount: Double
-    //Tag will eventually be changed to utilize dictionary
-    var tag: String
+    var tag: String //Tag will eventually be changed to utilize dictionary
     var person: Person
     var group: Group
+    
+    //MARK: Archiving Paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("receipts")
+    
+    //MARK: Types
+    
+    struct PropertyKey {
+        static let amount = "amount"
+        static let tag = "tag"
+        static let person = "person"
+        static let group = "group"
+        }
+    
+    //MARK: Initialization
     init( amount: Double , tag: String , person: Person , group: Group ){
+       
+        // Initialize stored properties.
         self.amount = amount
         self.tag = tag
         self.person = person
         self.group = group
     }
+    
+    //MARK: NSCoding
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(amount, forKey: PropertyKey.amount)
+        aCoder.encode(tag, forKey: PropertyKey.tag)
+        aCoder.encode(person, forKey: PropertyKey.person)
+        aCoder.encode(group, forKey: PropertyKey.group)
+    }
+
+    required convenience init?(coder aDecoder: NSCoder) {
+        
+        
+        let amount = aDecoder.decodeObject(forKey: PropertyKey.amount)
+        let tag = aDecoder.decodeObject(forKey: PropertyKey.tag)
+        let person = aDecoder.decodeObject(forKey: PropertyKey.person)
+        let group = aDecoder.decodeObject(forKey: PropertyKey.group)
+        
+        
+        // Must call designated initializer.
+        self.init(amount: amount as! Double, tag: tag as! String, person: person as! Person, group: group as! Group)
+        
+    }
 }
+
