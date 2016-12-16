f97e2b1590136906aeb1a1c8b39fae6849170489
FORD ADAM <758743@ehs-s-232-28>
2016-12-16 10:56:04 -0600

    Added the GUI for New Group and began coding to add Group/Person

--- groupPay/newGroupViewController.swift
+++ groupPay/newGroupViewController.swift
@@ -2,24 +2,50 @@
 //  newGroupViewController.swift
 //  groupPay
 //
-//  Created by FORD, ADAM on 12/15/16.
+//  Created by FORD, ADAM on 12/16/16.
 //  Copyright © 2016 FORD, ADAM. All rights reserved.
 //
-
+import Foundation
 import UIKit
-
+var people: [
 class newGroupViewController: UIViewController {
+    @IBOutlet weak var nameField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var phoneField: UITextField!
+    
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var nameDisplay: UILabel!
+    
+    @IBOutlet weak var phoneDisplay: UILabel!
+    
+    @IBOutlet weak var emailDisplay: UILabel!
+    
+    @IBOutlet weak var addPersonButton: UIButton!
+
+    @IBAction func addPersonButtonPressed(_ sender: UIButton) {
         
     }
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
