03c881714ac31d135006114c9774a918e9d4d317
WEST CARTER <738124@ehs-s-232-27.local>
2017-01-10 10:59:30 -0600

    added table view and groupArray to ViewController

--- groupPay/ViewController.swift
+++ groupPay/ViewController.swift
@@ -7,10 +7,29 @@
 //
 
 import UIKit
-
+var groupArray: [Group] = []
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var tableView: UITableView!
+    //how many sections are in the view
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    //table view will return int for how many rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return groupArray.count
+    }
+    
+    //what is in the table
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        var groupName  = groupArray[indexPath.row].name
+        cell.textLabel?.text = groupName
+        
+        return cell
+    }
     
     
     
