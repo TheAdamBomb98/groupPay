5a98be90584b9517d42d50ff56e8aac123946a04
strobelight6 <732012@ehs-s-232-26.district196.org>
2017-02-03 10:38:57 -0600

    theres a tableview connected on the group page now. it doesn’t contain people yet

--- groupPay/GroupPageController.swift
+++ groupPay/GroupPageController.swift
@@ -9,7 +9,29 @@
 import Foundation
 import UIKit
 
-class GroupPageController: UIViewController{
+class GroupPageController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    
+    @IBOutlet var peopleTable: UITableView!
+    
+    //how many sections are in the view
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //table view will return int for how many rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return allGroups.count
+    }
+    
+    //what is in the table
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        var personName  = allGroups[indexPath.row].name
+        cell.textLabel?.text = personName
+        return cell
+    }
 
  
     
