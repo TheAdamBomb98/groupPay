451095d7f06df574b86913f1a8834fc7595ca0eb
strobelight6 <732012@ehs-s-232-26.district196.org>
2017-02-07 10:46:48 -0600

    person page and file created and linked

---
+++ groupPay/PersonPageController.swift
@@ -0,0 +1,14 @@
+//
+//  PersonPageController.swift
+//  groupPay
+//
+//  Created by STROBEL, JOSEPH on 2/7/17.
+//  Copyright © 2017 FORD, ADAM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PersonPageController: UIViewController {
+    
+}
