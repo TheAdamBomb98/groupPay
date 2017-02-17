5f2beeba04388b59f724a78f4ca8d4677c40c391
strobelight6 <732012@ehs-s-232-26.district196.org>
2017-02-17 11:03:33 -0600

    collection view in the person page now

---
+++ groupPay/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  groupPay
+//
+//  Created by STROBEL, JOSEPH on 2/17/17.
+//  Copyright © 2017 FORD, ADAM. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+}
--- groupPay/PersonPageController.swift
+++ groupPay/PersonPageController.swift
@@ -9,11 +9,27 @@
 import Foundation
 import UIKit
 
-class PersonPageController: UIViewController {
+class PersonPageController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     var receivedPerson: Person?
     
     @IBOutlet weak var personLabel: UILabel!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        cell.label?.text = "hello"
+        cell.backgroundColor = UIColor.black
+        return cell
+    }
+    
+    //collection
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,4 +46,6 @@ class PersonPageController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 }
