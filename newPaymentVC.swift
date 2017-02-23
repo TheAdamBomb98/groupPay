978b505d6186ebdf6107f1c483917db4dc84a20b
jen5 <843121@ehs-s-232-23.district196.org>
2017-02-23 10:50:08 -0600

    added Payment page

---
+++ newPaymentVC.swift
@@ -0,0 +1,70 @@
+
+//
+//  newPaymentVC.swift
+//  groupPay
+//
+//  Created by BLANCHARD, JENNIFER on 2/13/17.
+//  Copyright © 2017 FORD, ADAM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import os.log
+
+class newPaymentVC: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource{
+    
+    
+    @IBOutlet weak var enterName: UILabel!
+    @IBOutlet weak var enterMoney: UITextField!
+    @IBOutlet weak var enterTag: UIPickerView!
+    let pickerData = ["Gas","Breakfast","Lunch","Dinner","Food","Snacks","Hotel","Tickets","Other"]
+    
+    var tempPerson = allGroups[0].people[0]
+    var tag = "test"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        allGroups = loadGroups()!
+        enterName.text = tempPerson.name
+        self.enterMoney.keyboardType = UIKeyboardType.numbersAndPunctuation
+        
+        enterTag.dataSource = self
+        enterTag.delegate = self
+    }
+    
+    
+    
+   
+    @IBAction func addPayment(_ sender: AnyObject) {
+        var amount = 0.0
+        amount = (Double)(enterMoney.text!)!
+        let person = allGroups[0].people[0]
+        let group = allGroups[0]
+        
+        let newPay = Receipt( amount: amount, tag: tag , person: person, group: group )
+        allGroups[0].people[0].transactions.append(newPay)
+        saveGroups()
+    }
+    
+    //PickerView Functions
+    //MARK: - Delegates and data sources
+    //MARK: Data Sources
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    //MARK: Delegate
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        tag = pickerData[0]
+    }
+    
+}
