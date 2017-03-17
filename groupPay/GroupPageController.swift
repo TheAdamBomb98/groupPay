e9858aadad6f6e8be9fb00a008403b7a7eccd323
strobelight6 <732012@ehs-s-232-26.district196.org>
2017-03-17 10:57:49 -0500

    remove delete person functionality

--- groupPay/GroupPageController.swift
+++ groupPay/GroupPageController.swift
@@ -62,17 +62,17 @@ class GroupPageController: UIViewController, UITableViewDelegate, UITableViewDat
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    /*func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if (editingStyle == UITableViewCellEditingStyle.delete) {
             allGroups[gotGroupIndex!].people.remove(at: indexPath.row)
             allGroups[indexOfGroup].people.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath],  with: UITableViewRowAnimation.automatic)
             saveGroups()
         
-            }
+            }*/
 
         }
-    }
+    
 
     
 
