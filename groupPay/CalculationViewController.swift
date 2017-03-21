e15590aa6ce9d5aa4aae3a18034fae4a3a537d7e
c35west <738124@ehs-s-232-27.district196.org>
2017-03-21 10:59:27 -0500

    tableview shows calculations

--- groupPay/CalculationViewController.swift
+++ groupPay/CalculationViewController.swift
@@ -8,21 +8,40 @@
 
 import Foundation
 import UIKit
-internal class CalculationViewController : UIViewController{
+internal class CalculationViewController : UIViewController, UITableViewDelegate, UITableViewDataSource{
     
     @IBOutlet weak var labelOfGroup: UILabel!
    
-    @IBOutlet weak var textOfCalculations: UITextView!
+    @IBOutlet weak var tableOfCalculations: UITableView!
+    
     var gotPersonIndex: Int?
     var gotGroupIndex: Int?
     var calculatedList: [Person] = []
     var fullSummary: String = ""
+    //how many sections are in the view
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //table view will return int for how many rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return calculationsSummary.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text =   calculationsSummary[indexPath.row]
+        return cell
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         gotGroupIndex = indexOfGroup
         gotPersonIndex = indexOfPerson
         labelOfGroup.text = ((allGroups[gotGroupIndex!].name) + " Calculations")
         var tempList = allGroups[gotGroupIndex!].people
+        calculationsSummary = []
         calculatedList = calculate(groupOfPeople: tempList)
     }
 }
