8ccd5d626c4570c27f6028b162edefabe6a115d2
strobelight6 <732012@ehs-s-232-26.district196.org>
2017-03-21 10:54:16 -0500

    app icons

--- groupPay/ViewController.swift
+++ groupPay/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         groupDate = dateSub
         //let label1 = UILabel(frame: CGRect(x: 15, y: 0, width: cell.frame.width/2, height: cell.frame.height))
         cell.textLabel?.text = groupName
-        let label2 = UILabel(frame: CGRect(x: cell.frame.width/2, y: 0, width: (cell.frame.width/2 + 80), height: cell.frame.height))
+        let label2 = UILabel(frame: CGRect(x: cell.frame.width/2, y: 0, width: (cell.frame.width/2 + 40), height: cell.frame.height))
         //label1.text = groupName
         label2.text = groupDate
         label2.textAlignment = .right
