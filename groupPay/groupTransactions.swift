da0c2a0a8fedf774cad4c3e172a48c3d22577d59
TheAdamBomb98 <758743@ehs-s-232-28.district196.org>
2017-05-03 10:22:36 -0500

    picker in groupTransactions

--- groupPay/groupTransactions.swift
+++ groupPay/groupTransactions.swift
@@ -8,12 +8,56 @@
 
 import Foundation
 import UIKit
-
-class groupTransactions: UIViewController {
+var groupTransactionsTag = "All Tags"
+class groupTransactions: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource {
+    
     @IBOutlet weak var pickCategory: UIPickerView!
     @IBOutlet weak var tableOfPayments: UITableView!
+    let enterTagPickerData = ["All Tags", "Gas","Breakfast","Lunch","Dinner","Food","Snacks","Hotel","Tickets","Other"]
+    @IBOutlet weak var tagChooser: UIPickerView!
+    var receipts: [Receipt] = []
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return enterTagPickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return enterTagPickerData[row]
+    }
     
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        groupTransactionsTag = enterTagPickerData[row]
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+   /* public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return enterTagPickerData.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+         return enterTagPickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inCompononet component: Int) {
+        groupTransactionsTag = enterTagPickerData[row]
+    }
+    */
     override func viewDidLoad() {
         func didReceiveMemoryWarning() {
             super.didReceiveMemoryWarning()
