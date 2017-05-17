222cb03ae2ffb7a196e35346df51e897b8dee424
TheAdamBomb98 <758743@ehs-s-232-28.district196.org>
2017-05-17 10:22:17 -0500

    another one

--- groupPay/CalculationViewController.swift
+++ groupPay/CalculationViewController.swift
@@ -83,7 +83,6 @@ internal class CalculationViewController : UIViewController, UITableViewDelegate
             //if let content = data {
                 //print("NSData: \(content)")
             //}
-    }
             // Generating the email controller.
             func configuredMailComposeViewController() -> MFMailComposeViewController {
                 let emailController = MFMailComposeViewController()
@@ -103,6 +102,7 @@ internal class CalculationViewController : UIViewController, UITableViewDelegate
                 
                 return emailController
             }
+    }
         /*
             func showSendMailErrorAlert() {
                 let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
